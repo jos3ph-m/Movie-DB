bb1744ef1f34659d17f8def0f33d484b77a44671
Joseph M <jmedina1985@hotmail.com>
2021-04-24 19:33:38 -0700

    Initial fetch

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Form from './SearchForm'
-import Movies from './Movies'
+import React from 'react';
+import Form from './SearchForm';
+import Movies from './Movies';
 const Home = () => {
-  return <h2>movie component</h2>
-}
+  return <main>movie component</main>;
+};
 
-export default Home
+export default Home;
