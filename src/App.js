867a1b4863e2fd202a81bbd32c484e6f35ada31c
Joseph M <jmedina1985@hotmail.com>
2021-04-24 19:12:44 -0700

    React router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import Home from './Home'
-import Movie from './SingleMovie'
+import Home from './Home';
+import Movie from './SingleMovie';
 
 function App() {
-  return <h2>movie DB starter</h2>
+  return (
+    <Switch>
+      <Router path="/" exact />
+    </Switch>
+  );
 }
 
-export default App
+export default App;
