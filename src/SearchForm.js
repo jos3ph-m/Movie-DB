d87afc49b47f59512c67009e7b36f811fc645f78
Joseph M <jmedina1985@hotmail.com>
2021-04-27 21:10:57 -0700

    Search form

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -5,7 +5,12 @@ const SearchForm = () => {
   return (
     <form className="search-form" onSubmit={(e) => e.preventDefault()}>
       <h2>search movies</h2>
-      <input type="text" className="form-input" />
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
     </form>
   );
 };
