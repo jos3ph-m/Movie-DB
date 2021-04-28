7d5a4b91ef8d0eb4cfa9e38d25dd512645a84801
Joseph M <jmedina1985@hotmail.com>
2021-04-27 21:09:08 -0700

    Search form

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { useGlobalContext } from './context';
 const SearchForm = () => {
   const { query, setQuery, error } = useGlobalContext();
-  return <form className="search-form">search form</form>;
+  return (
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      search form
+    </form>
+  );
 };
 
 export default SearchForm;
