bc32e91382c3994a3e376dc620b86985c739fe4c
Joseph M <jmedina1985@hotmail.com>
2021-04-27 21:18:28 -0700

    single movie

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
-import { API_ENDPOINT } from './context'
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { API_ENDPOINT, useGlobalContext } from './context';
 
 const SingleMovie = () => {
-  return <h2>single movie</h2>
-}
+  const { id } = useParams();
+  return <h2>single movie</h2>;
+};
 
-export default SingleMovie
+export default SingleMovie;
