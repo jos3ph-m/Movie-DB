2829525689898b6d3fc0a2207ff72108778e5b0a
Joseph M <jmedina1985@hotmail.com>
2021-04-28 20:52:33 -0700

    single movie

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -28,6 +28,14 @@ const SingleMovie = () => {
     return <div className="loading"></div>;
   }
 
+  if (error.show) {
+    return (
+      <div className="page-error">
+        <h1>{error.msg}</h1>
+      </div>
+    );
+  }
+
   return <h2>single movie</h2>;
 };
 
