53a38e26c28ac7e2a7c295e856c56adb445be481
Joseph M <jmedina1985@hotmail.com>
2021-04-29 21:55:29 -0700

    Refactor to useFetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
 
 const useFetch = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: '' });
+  const [movies, setMovies] = useState([]);
   return <div></div>;
 };
 
