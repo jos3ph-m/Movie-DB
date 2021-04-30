051fc1460f1afe08ce5a18f2524e7ae6ee9c1d84
Joseph M <jmedina1985@hotmail.com>
2021-04-29 22:00:47 -0700

    Refactor to useFetch

--- src/context.js
+++ src/context.js
@@ -1,11 +1,13 @@
 import React, { useState, useContext, useEffect } from 'react';
 // make sure to use https
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+import useFetch from './useFetch';
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [query, setQuery] = useState('predator');
+  const { isLoading, error, data: movies } = useFetch(`&s=${query}`);
 
   return (
     <AppContext.Provider value={{ isLoading, error, movies, query, setQuery }}>
