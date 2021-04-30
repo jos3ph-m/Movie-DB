eafc30c3283483d3736dd2c90bfdae5e37b3ca72
Joseph M <jmedina1985@hotmail.com>
2021-04-29 21:54:17 -0700

    Refactor to useFetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
 
 export const useFetch = () => {
   return <div></div>;
