cd397d023aa75caaa9943325c5fc166cf5b06d37
Joseph M <jmedina1985@hotmail.com>
2021-04-29 22:02:43 -0700

    Refactor to useFetch

--- src/context.js
+++ src/context.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react';
+import useFetch from './useFetch';
 // make sure to use https
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
-import useFetch from './useFetch';
 
 const AppContext = React.createContext();
 
