a418b5080cbe9674cc4d7e713b16ef7c85647bf2
Joseph M <jmedina1985@hotmail.com>
2021-04-29 21:56:21 -0700

    Refactor to useFetch

--- src/context.js
+++ src/context.js
@@ -5,31 +5,7 @@ export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState({ show: false, msg: '' });
-  const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState('predator');
-
-  const fetchMovies = async (url) => {
-    setIsLoading(true);
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      if (data.Response === 'True') {
-        setMovies(data.Search);
-        setError({ show: false, msg: '' });
-      } else {
-        setError({ show: true, msg: data.Error });
-      }
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchMovies(`${API_ENDPOINT}&s=${query}`);
-  }, [query]);
 
   return (
     <AppContext.Provider value={{ isLoading, error, movies, query, setQuery }}>
--- src/useFetch.js
+++ src/useFetch.js
@@ -5,7 +5,29 @@ const useFetch = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState({ show: false, msg: '' });
   const [movies, setMovies] = useState([]);
-  return <div></div>;
+
+  const fetchMovies = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.Response === 'True') {
+        setMovies(data.Search);
+        setError({ show: false, msg: '' });
+      } else {
+        setError({ show: true, msg: data.Error });
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`);
+  }, [query]);
+
+  return;
 };
 
 export default useFetch;
