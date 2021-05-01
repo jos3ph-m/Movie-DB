b47a78007af57f30de583828fe77bde934276ef0
Joseph M <jmedina1985@hotmail.com>
2021-04-30 22:04:19 -0700

    Refactor to useFetch

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -5,7 +5,7 @@ import useFetch from './useFetch';
 
 const SingleMovie = () => {
   const { id } = useParams();
-  const { isLoading, error, data: movie } = useFetch(`i=${id}`);
+  const { isLoading, error, data: movie } = useFetch(`&i=${id}`);
 
   if (error.show) {
     return (
