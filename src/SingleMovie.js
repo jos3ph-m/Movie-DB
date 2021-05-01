0cb6b02efc047ee638b5fdf3ecc38c4162058953
Joseph M <jmedina1985@hotmail.com>
2021-04-30 22:00:26 -0700

    Refactor to useFetch

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { API_ENDPOINT, useGlobalContext } from './context';
+import useFetch from './useFetch';
 
 const SingleMovie = () => {
   const { id } = useParams();
